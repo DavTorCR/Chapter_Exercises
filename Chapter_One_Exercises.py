c8ae38dccb4b70bb77ad6b20b6627f6117054408
David Toribio <hollow.prolific.vegabond@gmail.com>
2026-02-01 01:06:59 -0800

    starting, 12:56 am. testing, 3, 1:00 am. testing, 6, 1:03 am. testing, 7, 1:06 am.

--- Chapter_One_Exercises.py
+++ Chapter_One_Exercises.py
@@ -1,2 +1,3 @@
 print("Hello_World!")
 print("It only took 3 tries.")
+print("It took 6 tries, and I now know that the keyboard shortcuts dont seem to work quite right. I am going to keep using the GUI to intereact.")
